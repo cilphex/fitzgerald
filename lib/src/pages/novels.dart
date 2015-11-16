749563520153e63b56e20cb8eb04d50ec734f158
Craig Hammell <cilphex@gmail.com>
2015-11-16 12:39:09 -0800

    show breakage

--- lib/src/pages/novels.dart
+++ lib/src/pages/novels.dart
@@ -2,13 +2,10 @@ part of fitzgerald;
 
 class NovelsPage extends StatelessComponent {
   Widget build(BuildContext context) {
-    // return new Scaffold(
-    //   body: new Center(
-    //     child: new Text('Novels')
-    //   )
-    // );
-    return new Center(
-      child: new Text('Novels')
+    return new Scaffold(
+      body: new Center(
+        child: new Text('Novels')
+      )
     );
   }
 }
